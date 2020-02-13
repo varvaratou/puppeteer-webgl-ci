b123ea4aa6fbfce491864a107f62d16239b58e3e
munrocket@pm.me <munrocket@pm.me>
2020-02-13 20:20:56 +0400

    refactor

--- tests/puppeteer/deterministic-injection.js
+++ tests/puppeteer/deterministic-injection.js
@@ -1,46 +1,73 @@
-(function() {
+(  function() {
+	
 
-  // deterministic random
-  let seed = Math.PI / 4;
-  window.Math.random = function() {
-    const x = Math.sin(seed++) * 10000;
-    return x - Math.floor(x);
-  };
+	/* Deterministic random */
 
-  // deterministic timer
-  const now = function() { return frameId * 16; };
-  window.Date.now = now;
-  window.Date.prototype.getTime = now;
-  window.performance.wow = performance.now;
-  window.performance.now = now;
+	let seed = Math.PI / 4;
+	window.Math.random = function() {
+		
+		const x = Math.sin( seed++ ) * 10000;
+		return x - Math.floor( x );
 
-  // deterministic rAF
-  let frameId = 0;
-  window.chromeMaxFrameId = 1;
-  window.chromeRenderStarted = false;
-  window.chromeRenderFinished = false;
-  const rAF = window.requestAnimationFrame;
-  window.requestAnimationFrame = function(cb) {
-    if (!chromeRenderStarted) {
-      setTimeout(function() {
-        requestAnimationFrame(cb);
-      }, 50);
-    } else {
-      rAF(function() {
-        if (frameId++ < chromeMaxFrameId) {
-          cb(now());
-        } else {
-          chromeRenderFinished = true;
-        }
-      });
-    }
-  }
+	};
 
-  // semi-determitistic video
-  let play = HTMLVideoElement.prototype.play;
-  HTMLVideoElement.prototype.play = async function() {
-    this.addEventListener('timeupdate', () => this.pause());
-    play.call(this);
-  }
 
-}());
+	/* Deterministic timer */
+
+	const now = function() { return frameId * 16; };
+	
+	window.Date.now = now;
+	window.Date.prototype.getTime = now;
+	window.performance.wow = performance.now;
+	window.performance.now = now;
+
+
+	/* Deterministic rAF */
+
+	let frameId = 0;
+	window.chromeMaxFrameId = 1;
+	window.chromeRenderStarted = false;
+	window.chromeRenderFinished = false;
+	const rAF = window.requestAnimationFrame;
+	window.requestAnimationFrame = function( cb ) {
+		
+		if ( !chromeRenderStarted ) {
+			
+			setTimeout( function() {
+				
+				requestAnimationFrame( cb );
+		
+			}, 50 );
+
+		} else {
+			
+			rAF( function() {
+				
+				if ( frameId++ < chromeMaxFrameId ) {
+
+					cb( now() );
+
+				} else {
+
+					chromeRenderFinished = true;
+
+				}
+
+			} );
+
+		}
+
+	}
+
+
+	/* Semi-determitistic video */
+
+	let play = HTMLVideoElement.prototype.play;
+	HTMLVideoElement.prototype.play = async function() {
+
+		this.addEventListener( 'timeupdate', () => this.pause() );
+		play.call( this );
+
+	}
+
+}() );
